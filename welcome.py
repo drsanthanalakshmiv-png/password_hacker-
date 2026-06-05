776a573d271deefff5e15b2551ac5823e4e0b0a0
drsanthanalakshmiv-png <drsanthanalakshmi.v@drngpasc.ac.in>
2026-06-05 12:33:17 +0530

    Add welcome.py - a program to print welcome message

---
+++ welcome.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A simple program to print a welcome message.
+"""
+
+def main():
+    """Print a welcome message."""
+    print("Welcome!")
+
+if __name__ == "__main__":
+    main()
